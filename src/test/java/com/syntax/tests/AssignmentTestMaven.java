5d1115066cc28190df93cb9f62a6c61349d2d038
sayedabbasmosawi <samosawi7@gmail.com>
2018-12-08 02:44:36 -0500

    I added Assertion

--- src/test/java/com/syntax/tests/AssignmentTestMaven.java
+++ src/test/java/com/syntax/tests/AssignmentTestMaven.java
@@ -1,5 +1,8 @@
 package com.syntax.tests;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -8,17 +11,30 @@ import org.testng.annotations.Test;
 
 import com.syntax.utils.Base;
 
+import junit.framework.Assert;
+
 public class AssignmentTestMaven extends Base {
 
 	WebDriver driver;
+	String url;
 
 	@BeforeMethod
+	
 	public void launchTest() {
-		String url = "https://www.google.com/";
+		url = "https://www.google.com/";
 		System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void testRun() {
 		driver.get(url);
-
+		String actual = driver.getTitle();
+		String expected = "Google";
+		assertEquals(actual, expected);
+		System.out.println("Your test passed");
+		
 	}
 
 	@AfterMethod
